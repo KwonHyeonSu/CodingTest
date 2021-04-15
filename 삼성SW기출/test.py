bf89f50ed9e68a3786c42de015760ed712bf248d
KwonHyeonSu <hk3305@gmail.com>
2021-04-15 15:17:14 +0900

    hi

--- 삼성SW기출/test.py
+++ 삼성SW기출/test.py
@@ -1,93 +1,95 @@
-'''
-3 2
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 0 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2
+"""
+5
+100 200 300 400 200
+300 243 432 334 555
+999 111 0 999 333
+888 777 222 333 900
+100 200 300 400 500
 
-3 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1 2 3 4 5 6 7 8
-8 7 6 5 4 3 2 1
-1
+=>7501
 
-'''
+"""
 
 import sys
-
-N, Q = map(int, sys.stdin.readline().split())
-N_size = 2**N
-ice = [list(map(int, sys.stdin.readline().split())) for _ in range(N_size)]
-Q = list(map(int, sys.stdin.readline().split()))
-
+n = int(input())
+board = [[int(x) for x in input().split()] for _ in range(n)]
+ans = 0
 def pprint(arr):
-    print('------------------')
     n = len(arr)
+    print('---------------')
     for i in range(n):
         for j in range(n):
             print(arr[i][j], end = ' ')
         print()
 
-dir = [(0,1), (1,0), (-1,0), (0,-1)]
+left_move = [[0,0,2,0,0],[0,10,7,1,0],[5,'a',0,0,0],[0,10,7,1,0],[0,0,2,0,0]]
+down_move = [[0,0,0,0,0],[0,1,0,1,0],[2,7,0,7,2],[0,10,'a',10,0],[0,0,5,0,0]]
+right_move = [[0,0,2,0,0],[0,1,7,10,0],[0,0,0,'a',5],[0,1,7,10,0],[0,0,2,0,0]]
+up_move = [[0,0,5,0,0],[0,10,'a',10,0],[2,7,0,7,2],[0,1,0,1,0],[0,0,0,0,0]]
 
-def Rotate(L_size):
-    # 회전
-    for x in range(0, N_size, L_size):
-        for y in range(0, N_size, L_size):
-            temp = [ice[x+i][y:y+L_size] for i in range(L_size)]
+
+def spread(x, y, move, board, out):
+
+    temp = 0
+    nx, ny = x-2, y-2
+
+    for i in range(5):
+        for j in range(5):
+            if move[i][j] != 'a' and move[i][j] != 0:
+                if 0 <= i+nx < n and 0 <= j+ny < n:
+                    board[i+nx][j+ny] += board[x][y]*move[i][j]//100
+                else:
+                    out += board[x][y]*move[i][j]//100
+                temp += board[x][y]*move[i][j]//100
+            elif move[i][j] == 'a':
+                remain = (i,j)
+    if 0<=remain[0]+nx<n and 0<=remain[1]+ny<n:
+        board[remain[0]+nx][remain[1]+ny] += board[x][y]-temp
+    else:
+        out += board[x][y]-temp
+    board[x][y] = 0
+
+    return board, out
+    
+
+
+#left, down, right, up
+d = [(0,-1), (1,0), (0,1), (-1,0)]
+
+x, y = n//2, n//2
+times = 1
+flag = 0
+
+while flag != 1:
+    for i in range(4):
+        dx, dy = d[i]
+        for j in range(times):
+            x, y = x+dx, y+dy
+            # left
+            if i == 0:
+                board, ans = spread(x, y, left_move, board, ans)
+                
+            elif i == 1:
+            # down
+                board, ans = spread(x, y, down_move, board, ans)
+
+            elif i == 2:
+            # right
+                board, ans = spread(x, y, right_move, board, ans)
+
+            elif i == 3:
+
+            # up
+                board, ans = spread(x, y, up_move, board, ans)
+
+            if (x,y) == (0,0):
+                flag = 1
+                break
             
-            for i in range(L_size):
-                for j in range(L_size):
-                    ice[x+i][y+j] = temp[L_size-1-j][i]
 
-    # 인접한 칸 계산
-    cnt = [[0] * N_size for _ in range(N_size)]
-    for x in range(N_size):
-        for y in range(N_size):
-            for d in dir:
-                nx, ny = x + d[0], y + d[1]
-                if 0 <= nx < N_size and 0 <= ny < N_size and ice[nx][ny]:
-                    cnt[x][y] += 1
+        if i == 1 or i == 3:
+            times += 1
 
-    # 얼음 제거
-    for x in range(N_size):
-        for y in range(N_size):
-            if cnt[x][y] < 3 and ice[x][y] :
-                ice[x][y] -= 1
-
-#(x,y)가 속한 덩어리의 크기
-def dfs(x, y):
-    ret = 1
-    ice[x][y] = 0
-    for d in dir:
-        nx, ny = x+d[0], y+d[1]
-        if 0 <= nx < N_size and 0 <= ny < N_size and ice[nx][ny] != 0:
-            ret += dfs(nx, ny)
-    return ret
-
-
-
-for L in Q :
-    Rotate(2 ** L)
-
-print(sum(map(sum,ice)))
-
-ans = 0
-for x in range(N_size):
-    for y in range(N_size):
-        if ice[x][y]:
-            ans = max(ans, dfs(x, y))
-    
-
-print(ans)
-    
+        if flag == 1:
+            print(ans)
+            break
